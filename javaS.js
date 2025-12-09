42de293297cdd0c88b23a432b1c0cc16c38fff73
AsinthaK <asinthasenethmal@gmail.com>
2025-12-09 22:29:27 +0530

    first commit

---
+++ javaS.js
@@ -0,0 +1,29 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+
+    
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-content");
+    }
+
+    
+    event.currentTarget.classList.add("active-link");
+    
+    document.getElementById(tabname).classList.add("active-content");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
